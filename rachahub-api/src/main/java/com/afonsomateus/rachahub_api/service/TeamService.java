988d698011b813f2881cd52f53b75d43d79d53b9
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-03 23:21:14 -0300

    feat: create TeamService and create team method

---
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/TeamService.java
@@ -0,0 +1,28 @@
+package com.afonsomateus.rachahub_api.service;
+
+import org.springframework.stereotype.Service;
+
+import com.afonsomateus.rachahub_api.dto.team.TeamRequestDTO;
+import com.afonsomateus.rachahub_api.dto.team.TeamResponseDTO;
+import com.afonsomateus.rachahub_api.entity.Team;
+import com.afonsomateus.rachahub_api.mapper.TeamMapper;
+import com.afonsomateus.rachahub_api.repository.TeamRepository;
+
+@Service
+public class TeamService {
+	private final TeamRepository teamRepository;
+	private final TeamMapper teamMapper;
+	
+	public TeamService(TeamRepository repository, TeamMapper mapper) {
+		this.teamRepository = repository;
+		this.teamMapper = mapper;
+	}
+	
+	public TeamResponseDTO create(TeamRequestDTO dto) {
+		Team team = teamMapper.toEntity(dto);
+		team = teamRepository.save(team);
+		
+		return teamMapper.toResponse(team);
+	}
+}
+	
