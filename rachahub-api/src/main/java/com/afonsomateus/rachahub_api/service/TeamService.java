4688540257ac3eec457e3128a570af84f335405d
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-03 23:43:19 -0300

    feat: create getByIdMethod in service

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/TeamService.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/TeamService.java
@@ -1,12 +1,14 @@
 package com.afonsomateus.rachahub_api.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import com.afonsomateus.rachahub_api.dto.team.TeamRequestDTO;
 import com.afonsomateus.rachahub_api.dto.team.TeamResponseDTO;
 import com.afonsomateus.rachahub_api.entity.Team;
+import com.afonsomateus.rachahub_api.exceptions.ResourceNotFoundException;
 import com.afonsomateus.rachahub_api.mapper.TeamMapper;
 import com.afonsomateus.rachahub_api.repository.TeamRepository;
 
@@ -32,6 +34,14 @@ public class TeamService {
 			.stream()
 			.map((team) -> teamMapper.toResponse(team))
 			.toList();
+	}
+	
+	public TeamResponseDTO getById(UUID id) {
+		Team team = teamRepository
+			.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("Team not found."));
+		
+		return teamMapper.toResponse(team);
 	}
 }
 	
