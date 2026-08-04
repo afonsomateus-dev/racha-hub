56e7fd5196609a7282431999993b6954eadf72d0
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-04 16:28:08 -0300

    feat: create season controller and add create method

---
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/SeasonController.java
@@ -0,0 +1,31 @@
+package com.afonsomateus.rachahub_api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.afonsomateus.rachahub_api.dto.season.SeasonRequestDTO;
+import com.afonsomateus.rachahub_api.dto.season.SeasonResponseDTO;
+import com.afonsomateus.rachahub_api.service.SeasonService;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/seasons")
+public class SeasonController {
+	private final SeasonService seasonService;
+
+	public SeasonController(SeasonService seasonService) {
+		this.seasonService = seasonService;
+	}
+	
+	@PostMapping
+	public ResponseEntity<SeasonResponseDTO> create(@Valid @RequestBody SeasonRequestDTO dto) {
+		SeasonResponseDTO response = seasonService.create(dto);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+}
