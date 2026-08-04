beacf5a8723678d7e179074da93b33e3eb64a437
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-03 23:20:26 -0300

    fix: add component model to TeamMapper

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/mapper/TeamMapper.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/mapper/TeamMapper.java
@@ -6,7 +6,7 @@ import com.afonsomateus.rachahub_api.dto.team.TeamRequestDTO;
 import com.afonsomateus.rachahub_api.dto.team.TeamResponseDTO;
 import com.afonsomateus.rachahub_api.entity.Team;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface TeamMapper {
 	TeamResponseDTO toResponse(Team team);
 	Team toEntity(TeamRequestDTO teamDTO);
