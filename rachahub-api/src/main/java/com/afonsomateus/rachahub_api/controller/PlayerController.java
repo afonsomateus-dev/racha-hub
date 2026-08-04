f98a1d7a188cdcc9007ebf7dee154b4e9949e487
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-03 22:54:18 -0300

    feat: create delete player controller

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/PlayerController.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/PlayerController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,5 +52,12 @@ public class PlayerController {
 		PlayerResponseDTO player = playerService.update(id, dto);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(player);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable UUID id) {
+		playerService.delete(id);
+		
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
 	}
 }
