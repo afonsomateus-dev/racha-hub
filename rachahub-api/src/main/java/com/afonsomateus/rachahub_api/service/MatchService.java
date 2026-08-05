7817929b50259402e72408349c8f8f366a77e8cb
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-05 18:00:05 -0300

    feat: create match service and add create method

---
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/MatchService.java
@@ -0,0 +1,37 @@
+package com.afonsomateus.rachahub_api.service;
+
+import org.springframework.stereotype.Service;
+
+import com.afonsomateus.rachahub_api.dto.match.MatchRequestDTO;
+import com.afonsomateus.rachahub_api.dto.match.MatchResponseDTO;
+import com.afonsomateus.rachahub_api.entity.Match;
+import com.afonsomateus.rachahub_api.entity.Season;
+import com.afonsomateus.rachahub_api.exceptions.ResourceNotFoundException;
+import com.afonsomateus.rachahub_api.mapper.MatchMapper;
+import com.afonsomateus.rachahub_api.repository.MatchRepository;
+import com.afonsomateus.rachahub_api.repository.SeasonRepository;
+
+@Service
+public class MatchService {
+	private final MatchRepository matchRepository;
+	private final SeasonRepository seasonRepository;
+	private final MatchMapper matchMapper;
+	
+	public MatchService(MatchRepository matchRepository, SeasonRepository seasonRepository, MatchMapper mapper) {
+		this.matchRepository = matchRepository;
+		this.matchMapper = mapper;
+		this.seasonRepository = seasonRepository;
+	}
+	
+	public MatchResponseDTO create(MatchRequestDTO dto) {
+		Season season = seasonRepository.findById(dto.seasonId())
+			.orElseThrow(() -> new ResourceNotFoundException("Season not found."));
+		
+		Match match = matchMapper.toEntity(dto);
+		match.setSeason(season);
+		
+		match = matchRepository.save(match);
+		
+		return matchMapper.toResponse(match);
+	}
+}
