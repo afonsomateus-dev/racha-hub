6bc1206ac59cf70d2a50d4cdc74cbd4591680b97
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-05 17:59:12 -0300

    fix: add method to map season uuid to season entity

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/mapper/MatchMapper.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/mapper/MatchMapper.java
@@ -1,13 +1,27 @@
 package com.afonsomateus.rachahub_api.mapper;
 
+import java.util.UUID;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.afonsomateus.rachahub_api.dto.match.MatchRequestDTO;
 import com.afonsomateus.rachahub_api.dto.match.MatchResponseDTO;
 import com.afonsomateus.rachahub_api.entity.Match;
+import com.afonsomateus.rachahub_api.entity.Season;
 
 @Mapper(componentModel = "spring")
 public interface MatchMapper {
 	MatchResponseDTO toResponse(Match match);
+	
+	@Mapping(source = "seasonId", target = "season")
 	Match toEntity(MatchRequestDTO matchDTO);
+	
+	default Season map(UUID id) {
+		if (id == null) return null;
+		
+		Season season = new Season();
+		season.setId(id);
+		return season;
+	}
 }
