0c6da26e85876ea615cae2c440bdc9dbe6f78bef
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-05 19:58:14 -0300

    refac: remove validations decorators from MatchRequestDTO

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/dto/match/MatchRequestDTO.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/dto/match/MatchRequestDTO.java
@@ -3,11 +3,8 @@ package com.afonsomateus.rachahub_api.dto.match;
 import java.time.LocalDate;
 import java.util.UUID;
 
-import jakarta.validation.constraints.NotNull;
-
 public record MatchRequestDTO(
-	@NotNull 
 	UUID seasonId, 
-	@NotNull 
+	String title,
 	LocalDate date
 ) {}
