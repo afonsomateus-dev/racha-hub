37c6efed443848e5a9951f9705e8bbf0c97881a3
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-04 23:34:11 -0300

    feat: create update method in season service

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/SeasonService.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/SeasonService.java
@@ -3,6 +3,7 @@ package com.afonsomateus.rachahub_api.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.afonsomateus.rachahub_api.dto.season.SeasonRequestDTO;
@@ -11,6 +12,7 @@ import com.afonsomateus.rachahub_api.entity.Season;
 import com.afonsomateus.rachahub_api.exceptions.ResourceNotFoundException;
 import com.afonsomateus.rachahub_api.mapper.SeasonMapper;
 import com.afonsomateus.rachahub_api.repository.SeasonRepository;
+import com.afonsomateus.rachahub_api.utils.Helpers;
 
 @Service
 public class SeasonService {
@@ -40,6 +42,19 @@ public class SeasonService {
 	public SeasonResponseDTO findById(UUID id) {
 		Season season = seasonRepository.findById(id)
 			.orElseThrow(() -> new ResourceNotFoundException("Season not found."));
+		
+		return seasonMapper.toResponse(season);
+	}
+	
+	public SeasonResponseDTO update(UUID id, SeasonRequestDTO dto) {
+		Season season = seasonRepository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("Season not found."));
+		
+		Season seasonReceived = seasonMapper.toEntity(dto);
+		
+		BeanUtils.copyProperties(seasonReceived, season, Helpers.getNullPropertyNames(seasonReceived));
+		
+		seasonRepository.save(season);
 		
 		return seasonMapper.toResponse(season);
 	}
