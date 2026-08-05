d27dc77e69bbfb0911beaa7def331bb22f4d7211
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-05 19:59:25 -0300

    feat: add update method to match service

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/MatchService.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/MatchService.java
@@ -3,6 +3,7 @@ package com.afonsomateus.rachahub_api.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.afonsomateus.rachahub_api.dto.match.MatchRequestDTO;
@@ -13,6 +14,7 @@ import com.afonsomateus.rachahub_api.exceptions.ResourceNotFoundException;
 import com.afonsomateus.rachahub_api.mapper.MatchMapper;
 import com.afonsomateus.rachahub_api.repository.MatchRepository;
 import com.afonsomateus.rachahub_api.repository.SeasonRepository;
+import com.afonsomateus.rachahub_api.utils.Helpers;
 
 @Service
 public class MatchService {
@@ -48,6 +50,24 @@ public class MatchService {
 	public MatchResponseDTO findById(UUID id) {
 		Match match = matchRepository.findById(id)
 			.orElseThrow(() -> new ResourceNotFoundException("Match not found."));
+		
+		return matchMapper.toResponse(match);
+	}
+	
+	public MatchResponseDTO update(UUID id, MatchRequestDTO dto) {
+		if (dto.seasonId() != null) {
+			seasonRepository.findById(dto.seasonId())
+				.orElseThrow(() -> new ResourceNotFoundException("Season not found."));
+		}
+		
+		Match match = matchRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Season not found."));
+		
+		Match matchReceived = matchMapper.toEntity(dto);
+		
+		BeanUtils.copyProperties(matchReceived, match, Helpers.getNullPropertyNames(matchReceived));
+		
+		match = matchRepository.save(match);
 		
 		return matchMapper.toResponse(match);
 	}
