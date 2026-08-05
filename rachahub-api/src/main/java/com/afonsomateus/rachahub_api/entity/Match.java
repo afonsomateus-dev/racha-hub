ec3688c1198daddd0a69d049121783d170fbf04c
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-05 18:39:50 -0300

    refac: add title field to match entity

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/entity/Match.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/entity/Match.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,6 +33,10 @@ public class Match {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
+	
+	@NotBlank
+	@Column(nullable = false, length = 120)
+	private String title;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(
