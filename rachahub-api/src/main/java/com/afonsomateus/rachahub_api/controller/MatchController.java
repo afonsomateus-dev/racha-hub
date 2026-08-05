6be4f45b553c2f6f99c711bc7750f8d04cfcbf27
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-05 19:58:59 -0300

    feat: add update endopoint to match controller

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/MatchController.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/MatchController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,6 +43,12 @@ public class MatchController {
 	@GetMapping("/{id}")
 	public ResponseEntity<MatchResponseDTO> findById(@PathVariable UUID id) {
 		MatchResponseDTO response = matchService.findById(id); 
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+	
+	@PatchMapping("/{id}")
+	public ResponseEntity<MatchResponseDTO> update(@PathVariable UUID id, @Valid @RequestBody MatchRequestDTO dto) {
+		MatchResponseDTO response = matchService.update(id, dto); 
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 }
