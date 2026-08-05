cca3b02fb3d343f49de6f73a8130f4f85772b26d
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-05 17:58:07 -0300

    fix: add lombok decorators for getters, setters and constructors

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/entity/Match.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/entity/Match.java
@@ -3,7 +3,9 @@ package com.afonsomateus.rachahub_api.entity;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,15 +13,26 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "matches")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Match {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(
 		name = "season_id", 
 		nullable = false
