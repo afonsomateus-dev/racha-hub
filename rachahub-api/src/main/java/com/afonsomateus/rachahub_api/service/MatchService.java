514a0ff78f2be3d5a821e4c7d6a20bdea3b35cf8
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-05 19:38:16 -0300

    feat: add findById method to match service

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/MatchService.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/MatchService.java
@@ -1,6 +1,7 @@
 package com.afonsomateus.rachahub_api.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -42,5 +43,12 @@ public class MatchService {
 			.stream()
 			.map((match) -> matchMapper.toResponse(match))
 			.toList();
+	}
+	
+	public MatchResponseDTO findById(UUID id) {
+		Match match = matchRepository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("Match not found."));
+		
+		return matchMapper.toResponse(match);
 	}
 }
