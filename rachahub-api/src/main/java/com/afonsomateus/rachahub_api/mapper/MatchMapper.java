ee1845d9926832658b6cfe2c3e7853251dbf2d9b
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-04 23:44:27 -0300

    fix: add component model to match mapper

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/mapper/MatchMapper.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/mapper/MatchMapper.java
@@ -6,7 +6,7 @@ import com.afonsomateus.rachahub_api.dto.match.MatchRequestDTO;
 import com.afonsomateus.rachahub_api.dto.match.MatchResponseDTO;
 import com.afonsomateus.rachahub_api.entity.Match;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface MatchMapper {
 	MatchResponseDTO toResponse(Match match);
 	Match toEntity(MatchRequestDTO matchDTO);
