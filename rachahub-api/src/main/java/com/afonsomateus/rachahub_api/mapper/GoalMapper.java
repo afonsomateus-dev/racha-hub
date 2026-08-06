9b51c176ce038ad5d8068720707c27f50996a0c0
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-05 23:59:53 -0300

    fix: create methods to map UUID to entities

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/mapper/GoalMapper.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/mapper/GoalMapper.java
@@ -1,8 +1,50 @@
 package com.afonsomateus.rachahub_api.mapper;
 
-import org.mapstruct.Mapper;
+import java.util.UUID;
 
-@Mapper
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.afonsomateus.rachahub_api.dto.goal.GoalRequestDTO;
+import com.afonsomateus.rachahub_api.dto.goal.GoalResponseDTO;
+import com.afonsomateus.rachahub_api.entity.Goal;
+import com.afonsomateus.rachahub_api.entity.Match;
+import com.afonsomateus.rachahub_api.entity.Player;
+import com.afonsomateus.rachahub_api.entity.Team;
+
+@Mapper(componentModel = "spring")
 public interface GoalMapper {
+	GoalResponseDTO toResponse(Goal goal);
 	
+	@Mapping(source = "playerId", target = "player")
+	@Mapping(source = "matchId", target = "match")
+	@Mapping(source = "teamId", target = "team")
+	Goal toEntity(GoalRequestDTO dto);
+	
+	default Player mapPlayer(UUID id) {
+		if (id == null) return null;
+		
+		Player player = new Player();
+		player.setId(id);
+		
+		return player;
+	}
+	
+	default Match mapMatch(UUID id) {
+		if (id == null) return null;
+		
+		Match match = new Match();
+		match.setId(id);
+		
+		return match;
+	}
+	
+	default Team mapTeam(UUID id) {
+		if (id == null) return null;
+		
+		Team team = new Team();
+		team.setId(id);
+		
+		return team;
+	}
 }
