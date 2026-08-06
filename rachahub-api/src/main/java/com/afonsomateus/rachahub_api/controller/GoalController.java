c3fa1729ee09357e669e915970aab91d43f36b74
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-06 00:00:56 -0300

    feat: create goal controller and add create goal endpoint

---
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/GoalController.java
@@ -0,0 +1,29 @@
+package com.afonsomateus.rachahub_api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.afonsomateus.rachahub_api.dto.goal.GoalRequestDTO;
+import com.afonsomateus.rachahub_api.dto.goal.GoalResponseDTO;
+import com.afonsomateus.rachahub_api.service.GoalService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/goals")
+@RequiredArgsConstructor
+public class GoalController {
+	private final GoalService goalService;
+	
+	@PostMapping
+	public ResponseEntity<GoalResponseDTO> create(@Valid @RequestBody GoalRequestDTO dto) {
+		GoalResponseDTO response = goalService.create(dto);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+}
