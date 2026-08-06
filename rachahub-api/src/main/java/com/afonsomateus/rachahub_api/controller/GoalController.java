d93ba3058664d7e7ad11ddb765aa0cbf431c43f2
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-06 00:05:52 -0300

    feat: add findAll endpoint to goal controller

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/GoalController.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/GoalController.java
@@ -1,7 +1,10 @@
 package com.afonsomateus.rachahub_api.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +26,12 @@ public class GoalController {
 	@PostMapping
 	public ResponseEntity<GoalResponseDTO> create(@Valid @RequestBody GoalRequestDTO dto) {
 		GoalResponseDTO response = goalService.create(dto);
-		
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<GoalResponseDTO>> findAll() {
+		List<GoalResponseDTO> response = goalService.findAll();
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 }
