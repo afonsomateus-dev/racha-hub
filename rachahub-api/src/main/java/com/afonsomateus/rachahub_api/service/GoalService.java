c495362492006f516c102f871dbf957c21cfac42
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-06 00:00:25 -0300

    feat: create goal service and add create goal method

---
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/GoalService.java
@@ -0,0 +1,48 @@
+package com.afonsomateus.rachahub_api.service;
+
+import org.springframework.stereotype.Service;
+
+import com.afonsomateus.rachahub_api.dto.goal.GoalRequestDTO;
+import com.afonsomateus.rachahub_api.dto.goal.GoalResponseDTO;
+import com.afonsomateus.rachahub_api.entity.Goal;
+import com.afonsomateus.rachahub_api.entity.Match;
+import com.afonsomateus.rachahub_api.entity.Player;
+import com.afonsomateus.rachahub_api.entity.Team;
+import com.afonsomateus.rachahub_api.exceptions.ResourceNotFoundException;
+import com.afonsomateus.rachahub_api.mapper.GoalMapper;
+import com.afonsomateus.rachahub_api.repository.GoalRepository;
+import com.afonsomateus.rachahub_api.repository.MatchRepository;
+import com.afonsomateus.rachahub_api.repository.PlayerRepository;
+import com.afonsomateus.rachahub_api.repository.TeamRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GoalService {
+	private final GoalRepository goalRepository;
+	private final PlayerRepository playerRepository;
+	private final MatchRepository matchRepository;
+	private final TeamRepository teamRepository;
+	private final GoalMapper goalMapper;
+	
+	public GoalResponseDTO create(GoalRequestDTO dto) {
+		Player player = playerRepository.findById(dto.playerId())
+			.orElseThrow(() -> new ResourceNotFoundException("Player not found."));
+		
+		Match match = matchRepository.findById(dto.matchId())
+			.orElseThrow(() -> new ResourceNotFoundException("Match not found."));
+		
+		Team team = teamRepository.findById(dto.teamId())
+			.orElseThrow(() -> new ResourceNotFoundException("Team not found."));
+		
+		Goal goal = goalMapper.toEntity(dto);
+		goal.setPlayer(player);
+		goal.setMatch(match);
+		goal.setTeam(team);
+		
+		goal = goalRepository.save(goal);
+		
+		return goalMapper.toResponse(goal);
+	}
+}
