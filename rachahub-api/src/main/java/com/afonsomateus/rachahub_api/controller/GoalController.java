d81ee3b5491724f48156ac09b4998219e5b5f699
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-07 17:49:50 -0300

    feat: add delete method in goal controller

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/GoalController.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/GoalController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,5 +50,11 @@ public class GoalController {
 	public ResponseEntity<GoalResponseDTO> update(@PathVariable UUID id, @Valid @RequestBody GoalRequestDTO dto) {
 		GoalResponseDTO response = goalService.update(id, dto);;
 		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable UUID id) {
+		goalService.delete(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
 	}
 }
