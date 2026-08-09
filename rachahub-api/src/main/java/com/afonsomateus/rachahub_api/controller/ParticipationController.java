bf9aedc1b0f466ae8ff2ecea2dcf94cd52df35c6
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-09 14:49:10 -0300

    feat: add find by id endpoint to participation controller

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/ParticipationController.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/ParticipationController.java
@@ -1,10 +1,12 @@
 package com.afonsomateus.rachahub_api.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,4 +36,10 @@ public class ParticipationController {
 		List<ParticipationResponseDTO> response = participationService.findAll();
 		return ResponseEntity.status(HttpStatus.OK).body(response);
  	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ParticipationResponseDTO> findById(@PathVariable UUID id) {
+		ParticipationResponseDTO response = participationService.findById(id);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
 }
