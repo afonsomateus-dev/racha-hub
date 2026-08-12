eb45e1dd4d100c19d94b01c49a9131aa93aff3b4
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-11 22:59:33 -0300

    feat: create team match service and add create endpoint

---
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/TeamMatchService.java
@@ -0,0 +1,40 @@
+package com.afonsomateus.rachahub_api.service;
+
+import org.springframework.stereotype.Service;
+
+import com.afonsomateus.rachahub_api.dto.teamMatch.TeamMatchRequestDTO;
+import com.afonsomateus.rachahub_api.dto.teamMatch.TeamMatchResponseDTO;
+import com.afonsomateus.rachahub_api.entity.Match;
+import com.afonsomateus.rachahub_api.entity.Team;
+import com.afonsomateus.rachahub_api.entity.TeamMatch;
+import com.afonsomateus.rachahub_api.exceptions.ResourceNotFoundException;
+import com.afonsomateus.rachahub_api.mapper.TeamMatchMapper;
+import com.afonsomateus.rachahub_api.repository.MatchRepository;
+import com.afonsomateus.rachahub_api.repository.TeamMatchRepository;
+import com.afonsomateus.rachahub_api.repository.TeamRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TeamMatchService {
+	private final TeamMatchRepository teamMatchRepository;
+	private final TeamRepository teamRepository;
+	private final MatchRepository matchRepository;
+	private final TeamMatchMapper teamMatchMapper;
+	
+	public TeamMatchResponseDTO create(TeamMatchRequestDTO dto) {
+		Team team = teamRepository.findById(dto.teamId())
+				.orElseThrow(() -> new ResourceNotFoundException("Team not found."));
+		
+		Match match = matchRepository.findById(dto.matchId())
+				.orElseThrow(() -> new ResourceNotFoundException("Match not found."));
+		
+		TeamMatch teamMatch = new TeamMatch();
+		teamMatch.setMatch(match);
+		teamMatch.setTeam(team);
+		teamMatch.setGoals(dto.goals());
+		
+		return teamMatchMapper.toResponse(teamMatchRepository.save(teamMatch));
+	}
+}
