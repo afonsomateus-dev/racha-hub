332909371a204eab2e703f753f151b003a93ec47
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-15 15:01:41 -0300

    fix: remove null validators from TeamMatchRequestDTO

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/dto/teamMatch/TeamMatchRequestDTO.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/dto/teamMatch/TeamMatchRequestDTO.java
@@ -6,10 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record TeamMatchRequestDTO(
-	@NotNull
 	UUID teamId,
-	@NotNull
 	UUID matchId,
-	@PositiveOrZero
-	int goals
+	Integer goals
 ) {}
