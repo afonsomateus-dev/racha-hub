feab90b61c6919ed68d0750e50a403825ef9b5d1
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-17 11:30:32 -0300

    feat: create assist controller and add create endpoint

---
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/AssistController.java
@@ -0,0 +1,28 @@
+package com.afonsomateus.rachahub_api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.afonsomateus.rachahub_api.dto.assist.AssistRequestDTO;
+import com.afonsomateus.rachahub_api.dto.assist.AssistResponseDTO;
+import com.afonsomateus.rachahub_api.service.AssistService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/assists")
+@RequiredArgsConstructor
+public class AssistController {
+	private final AssistService assistService;
+	
+	@PostMapping
+	public ResponseEntity<AssistResponseDTO> create(@Valid @RequestBody AssistRequestDTO dto) {
+		AssistResponseDTO response = assistService.create(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+}
