5c9b1a0afb579e35986ce10cfabc11094f1a5e79
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-17 10:24:28 -0300

    feat: add delete endpoint to team match controller

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/TeamMatchController.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/controller/TeamMatchController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -52,5 +53,11 @@ public class TeamMatchController {
 	) {
 		TeamMatchResponseDTO response = teamMatchService.update(teamId, matchId, dto);
 		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+	
+	@DeleteMapping("/teams/{teamId}/matches/{matchId}")
+	public ResponseEntity<Void> delete(@PathVariable UUID teamId, @PathVariable UUID matchId) {
+		teamMatchService.delete(teamId, matchId);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
 	}
 }
