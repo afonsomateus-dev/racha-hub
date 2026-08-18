32e9aaa1e518c93cdb2f0761180975f9ff5df68e
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-18 10:11:49 -0300

    fix: remove NotNull constraints from PlayerParticipationRequestDTO

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/dto/playerParticipation/PlayerParticipationRequestDTO.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/dto/playerParticipation/PlayerParticipationRequestDTO.java
@@ -2,11 +2,7 @@ package com.afonsomateus.rachahub_api.dto.playerParticipation;
 
 import java.util.UUID;
 
-import jakarta.validation.constraints.NotNull;
-
 public record PlayerParticipationRequestDTO(
-	@NotNull
 	UUID playerId,
-	@NotNull
 	UUID participationId
 ) {}
