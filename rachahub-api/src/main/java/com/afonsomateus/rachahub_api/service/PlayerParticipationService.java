4697ded24f6c6748762087d9a5064e84476e0d52
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-18 10:12:40 -0300

    feat: add update method to player participation service

--- rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/PlayerParticipationService.java
+++ rachahub-api/src/main/java/com/afonsomateus/rachahub_api/service/PlayerParticipationService.java
@@ -11,6 +11,7 @@ import com.afonsomateus.rachahub_api.entity.Participation;
 import com.afonsomateus.rachahub_api.entity.Player;
 import com.afonsomateus.rachahub_api.entity.PlayerParticipation;
 import com.afonsomateus.rachahub_api.entity.PlayerParticipationId;
+import com.afonsomateus.rachahub_api.exceptions.ResourceAlreadyExistsException;
 import com.afonsomateus.rachahub_api.exceptions.ResourceNotFoundException;
 import com.afonsomateus.rachahub_api.mapper.PlayerParticipationMapper;
 import com.afonsomateus.rachahub_api.repository.ParticipationRepository;
@@ -58,5 +59,41 @@ public class PlayerParticipationService {
 			.orElseThrow(() -> new ResourceNotFoundException("Player participation not found."));
 		
 		return playerParticipationMapper.toResponse(playerParticipation);
+	}
+	
+	public PlayerParticipationResponseDTO update(UUID playerId, UUID participationId, PlayerParticipationRequestDTO dto) {
+		PlayerParticipationId oldId = new PlayerParticipationId(playerId, participationId);
+		PlayerParticipation playerParticipation = playerParticipationRepository.findById(oldId)
+			.orElseThrow(() -> new ResourceNotFoundException("Player participation not found."));
+		
+		UUID newPlayerId = dto.playerId() != null ? dto.playerId() : playerId;
+		UUID newParticipationId = dto.participationId() != null ? dto.participationId() : participationId;
+		
+		if (playerId.equals(newPlayerId) && playerId.equals(newParticipationId)) {
+			return playerParticipationMapper.toResponse(playerParticipation);
+		}
+		
+		Player player = playerRepository
+			.findById(newPlayerId)
+			.orElseThrow(() -> new ResourceNotFoundException("Player not found."));
+		
+		Participation participation = participationRepository
+			.findById(newParticipationId)
+			.orElseThrow(() -> new ResourceNotFoundException("Participation not found."));
+		
+		PlayerParticipationId newId = new PlayerParticipationId(newPlayerId, newParticipationId);
+		
+		if (playerParticipationRepository.existsById(newId)) {
+			throw new ResourceAlreadyExistsException("Player participation already exists");
+		}
+		
+		playerParticipationRepository.deleteById(oldId);
+		
+		PlayerParticipation playerParticipationToSave = new PlayerParticipation();
+		playerParticipationToSave.setId(newId);
+		playerParticipationToSave.setPlayer(player);
+		playerParticipationToSave.setParticipation(participation);
+		
+		return playerParticipationMapper.toResponse(playerParticipationToSave);
 	}
 }
