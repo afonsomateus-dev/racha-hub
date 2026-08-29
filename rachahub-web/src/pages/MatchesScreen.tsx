0d7a0176d62b69743e0aec115b15bd221b33cdda
Afonso Mateus <afonsomateus.dev@gmail.com>
2026-08-29 14:04:51 -0300

    feat: add MatchesScreen structure

--- rachahub-web/src/pages/MatchesScreen.tsx
+++ rachahub-web/src/pages/MatchesScreen.tsx
@@ -1,5 +1,14 @@
+import { MatchInfoCard } from "../components/match/MatchInfoCard";
+import { MATCHES } from "../constants";
+
 export const MatchesScreen = () => {
-  return (
-    <h1>Matches</h1>
-  );
+    return (
+      <div className="flex flex-col gap-4 pb-4">
+        <div className="flex items-baseline justify-between pt-1">
+          <h1 className="text-3xl font-black text-white font-title">PARTIDAS</h1>
+          <span className="text-xs text-white/25">{MATCHES.length} jogos</span>
+        </div>
+        <MatchInfoCard matches={MATCHES} />
+      </div>
+    );
 }
